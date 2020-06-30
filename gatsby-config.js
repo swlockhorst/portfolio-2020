c567a6e6f918b61350094ebbf8a4acab855f8509
Steven Lockhorst <swlockhorst@gmail.com>
2020-06-30 16:34:32 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
-require("dotenv").config({
-  path: `.env`,
-});
+// require("dotenv").config({
+//   path: `.env`,
+// });
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
